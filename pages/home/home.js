1a4a02474376c821b46735adf9f7d450f65c22b4
WLB <wlb@example.com>
2020-06-27 18:45:33 +0800

    动态绑定（for循环和事件点击）

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,3 +1,21 @@
 Page({
-  
+  data:{
+    name:"WLB",
+    students:[
+      {id:1,name:"name1",age:"20"},
+      {id:2,name:"name2",age:"20"},
+      {id:3,name:"name3",age:"20"}
+    ],
+    count : 0
+  },
+  add(){
+    this.setData({
+      count: this.data.count +1
+    });
+  },
+  sub(){
+    this.setData({
+      count: this.data.count -1
+    });
+  }
 })

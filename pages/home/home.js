b7b7e75dd9b9ac7d5f5174da1b760efb10793841
WLB <wlb@example.com>
2020-06-29 14:34:41 +0800

    报表和折线图

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,21 +1,100 @@
+var wxCharts = require("../../utils/wxcharts.js");
+var app = getApp();
 Page({
-  data:{
-    name:"WLB",
-    students:[
-      {id:1,name:"name1",age:"20"},
-      {id:2,name:"name2",age:"20"},
-      {id:3,name:"name3",age:"20"}
-    ],
-    count : 0
-  },
-  add(){
-    this.setData({
-      count: this.data.count +1
-    });
-  },
-  sub(){
-    this.setData({
-      count: this.data.count -1
-    });
-  }
-})
+    data :{
+        lineChart : null,
+        windowWidth : 320,
+        list:["0时","4时","8时","12时","16时","20时"],
+        title:["成本","利润"],
+        datas:[]
+    },
+    touchHandler: function (e) {
+        this.data.lineChart.showToolTip(e, {
+            format: function (item, category) {
+                return category + ' ' + item.name + ':' + item.data 
+            }
+        });
+    },    
+    createData: function () {
+        var data = [];
+        for (var i = 0; i < 6; i++) {
+            data.push(Math.floor(Math.random()*(20-10))+10);
+        }
+        return data;
+    },
+    updateData: function () {
+        var data1 = this.createData();
+        var data2 = this.createData();
+        this.setData({
+            datas:[data1,data2]
+        });
+        var series = [{
+            name: '成本',
+            data: data1,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        },
+        {
+            name: '利润',
+            data: data2,
+            format: function (val, name) {
+                return val.toFixed(2) + '万';
+            }
+        }
+    ];
+        this.data.lineChart.updateData({
+            categories: this.data.list,
+            series: series
+        });
+    },
+    onLoad: function (e) {
+        try {
+            this.setData({
+                windowWidth : wx.getSystemInfoSync().windowWidth
+            });
+        } catch (e) {
+            console.error('getSystemInfoSync failed!');
+        }
+        
+        this.setData({
+            datas :[this.createData(),this.createData()]
+        });
+        this.data.lineChart = new wxCharts({
+            canvasId: 'lineCanvas',
+            type: 'line',
+            categories: this.data.list,
+            animation: true,
+            series: [{
+                name: '成本',
+                data: this.data.datas[0],
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            }, {
+                name: '利润',
+                data: this.data.datas[1],
+                format: function (val, name) {
+                    return val.toFixed(2) + '万';
+                }
+            }],
+            xAxis: {
+                disableGrid: true
+            },
+            yAxis: {
+                title: '成交金额 (万元)',
+                format: function (val) {
+                    return val.toFixed(2);
+                },
+                min: 0
+            },
+            width: this.data.windowWidth,
+            height: 200,
+            dataLabel: false,
+            dataPointShape: true,
+            extra: {
+                lineStyle: 'curve'
+            }
+        });
+    }
+});
